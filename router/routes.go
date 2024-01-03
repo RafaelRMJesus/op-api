964ddf8883ffd3fe505ea3c496166368c1fcf734
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-01-03 00:20:22 -0300

    feat: reestruct handlers

--- router/routes.go
+++ router/routes.go
@@ -1,17 +1,17 @@
 package router
 
 import (
-	"github.com/RafaelRMJesus/op-api/handler"
+	"github.com/RafaelRMJesus/op-api/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.GetOpeningHandler)
-		v1.POST("/opening", handler.CreateOpeningHandler)
-		v1.PUT("/opening", handler.UpdateOpeningHandler)
-		v1.DELETE("/opening", handler.DeleteOpeningHandler)
-		v1.GET("/openings", handler.GetMultipleOpeningsHandler)
+		v1.GET("/opening", handlers.GetOpeningHandler)
+		v1.POST("/opening", handlers.CreateOpeningHandler)
+		v1.PUT("/opening", handlers.UpdateOpeningHandler)
+		v1.DELETE("/opening", handlers.DeleteOpeningHandler)
+		v1.GET("/openings", handlers.ListOpeningsHandler)
 	}
 }
