bcc23c4939fa9521d4c322fa91c14eee2d7f09e4
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-01-12 17:33:21 -0300

    feat: add request and response handlers

--- handlers/createOpening.go
+++ handlers/createOpening.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/RafaelRMJesus/op-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,12 +14,24 @@ func CreateOpeningHandler(ctx *gin.Context) {
 
 	if err := request.Validate(); err != nil {
 		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, 400, err.Error())
 		return
 	}
 
-	if err := db.Create(&request).Error; err != nil {
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
 		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
 		return
 	}
+	sendSuccess(ctx, "create-opening", opening)
 
 }
