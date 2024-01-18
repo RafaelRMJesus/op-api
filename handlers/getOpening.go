3a6da9283e5fc06a2acd1bed456d7ac7f72aec9b
RafaelRMJesus <rafaelrocha191919@gmail.com>
2024-01-18 13:19:55 -0300

    feat: get and list openings endpoints

--- handlers/getOpening.go
+++ handlers/getOpening.go
@@ -1,13 +1,23 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/RafaelRMJesus/op-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, 400, errParamIsReq("id", "queryParam").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id %s not found", id))
+		return
+	}
+	sendSuccess(ctx, "get-opening", opening)
 }
--- handlers/listOpenings.go
+++ handlers/listOpenings.go
@@ -1,13 +1,17 @@
 package handler
 
 import (
-	"net/http"
-
+	"github.com/RafaelRMJesus/op-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET multiple openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, 500, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
